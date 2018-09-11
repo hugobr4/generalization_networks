5b465ce2aa67ba3e80eef21e2f47f715dbcb4d32
Vini Salazar <viniws@gmail.com>
2018-09-11 01:58:20 -0300

    implement random

--- example.py
+++ example.py
@@ -1,24 +1,34 @@
 '''
 Employing NetworkX to model stimuli response networks.
 '''
-
+import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
+G = nx.DiGraph()
 
-G.add_node(1, name="stimulus")
+stimulus = random.random()
+conditioning = 0.2
+
+G.add_node(1, name="stimulus", weight=stimulus)
 G.add_node(2, name="detection")
 G.add_node(3, name="brain state")
-G.add_node(4, name="redund check")
-G.add_node(5, name="yes", weight=1)
-G.add_node(6, name="probably", weight=0.66)
-G.add_node(7, name="unlikely", weight=0.33)
-G.add_node(8, name="no")
-G.add_node(9, name="valence computing")
-G.add_node(10, name="behaviour")
-G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (4, 6), (4, 7), (4, 8),
-                  (5, 10), (6, 9), (7, 9), (9, 10)])
+G.add_node(4, name="redund check", weight = stimulus + stimulus * conditioning)
+#G.add_node(7, name="unlikely", weight=0.33)
+G.add_edges_from([(1, 2), (2, 3), (3, 4)])
+
+if stimulus > 0.66:
+    G.add_node(5, name="yes", weight=1)
+    G.add_node(10, name="behaviour")
+    G.add_edges_from([(4, 5), (5, 10)])
+elif 0.33 < stimulus <= 0.66:
+    G.add_node(6, name="probably", weight=0.66)
+    G.add_node(9, name="valence computing")
+    G.add_node(10, name="behaviour")
+    G.add_edges_from([(4, 6), (6, 9), (9, 10)])
+else:
+    G.add_node(8, name="no")
+    G.add_edges_from([(4, 8)])
 
 
 pos = nx.spring_layout(G)
@@ -26,7 +36,6 @@ pos = nx.spring_layout(G)
 labels = nx.get_node_attributes(G, name="name")
 
 nx.draw_networkx_nodes(G, pos,
-                       nodelist=[i for i in range(1,11)],
                        node_color='b',
                        alpha=0.6)
 
@@ -39,20 +48,7 @@ plt.show()
 
 
 
-
-
-# e = [('a', 'b', 0.3), ('b', 'c', 0.9), ('a', 'c', 0.5), ('c', 'd', 1.2)]
-# G.add_weighted_edges_from(e)
-#
-# print(nx.dijkstra_path(G, 'a', 'd'))
-# ['a', 'c', 'd']
-#
-# G = nx.cubical_graph()
-#
-# plt.subplot(121)
-# nx.draw(G)
-#
-# plt.subplot(122)
-# nx.draw(G, pos=nx.circular_layout(G), nodecolor='r', edge_color='b')
-#
-# plt.show()
+'''
+G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (4, 6), (4, 7), (4, 8),
+                  (5, 10), (6, 9), (7, 9), (9, 10)])
+'''
