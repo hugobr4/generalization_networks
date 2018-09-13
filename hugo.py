ba5475aebc7d03ab26f118bd4b008fd63b287c9c
Vini Salazar <viniws@gmail.com>
2018-09-12 22:02:44 -0300

    hugooooO

--- hugo.py
+++ hugo.py
@@ -8,28 +8,26 @@ import matplotlib.pyplot as plt
 
 def initial_variables():
     # 430-770 is the human vision spectrum in THz
-    color = 520
-    #color = random.randrange(430, 771)
+    #color = 500
+    color = random.randrange(430, 771)
 
     # We want a variable for the intensity of the conditioning
-    #c_intensity = 1.0
     c_intensity = random.uniform(0.0, 1.0)
 
     # We will derive two coefficients from the conditioning_intensity
     # Sensibility will range from 1.0 to 1.2
     #sensi = 1.2
-    sensi = 1 + c_intensity/5
+    sensi = 0.9 + c_intensity/10
     # Valence will range from 1.0 to 1.5
     #valence = 1.5
-    valence = 1 + c_intensity/2
+    valence = 0.9 + c_intensity/10
 
     # This is the color for which our subject is conditioned
-    c_color = 500
-    #c_color = random.randrange(430, 771)
+    c_color = random.randrange(color-40, color+40)
 
     # This is the degree of "proximity" from the stimulus to the conditioned color
     if abs(c_color - color) < 40:
-        proximity = 1.0 - ((abs(c_color - color) / 40)) ** 0.5
+        proximity = 1.0 - ((abs(c_color - color) / 40)) ** 2
     else:
         proximity = 0.0
 
@@ -40,7 +38,7 @@ def main():
     G = nx.DiGraph()
 
     def draw():
-        pos = nx.spring_layout(G)
+        pos = nx.circular_layout(G)
         labels = nx.get_node_attributes(G, name="name")
         nx.draw_networkx_nodes(G, pos,
                                node_color='b',
@@ -55,7 +53,7 @@ def main():
               "Valence: {}\n".format(valence),
               "Conditioning Color: {} THz\n".format(c_color),
               "Proximity: {}\n".format(proximity),
-              "Redundance: {}".format(redundance)
+              "Response: {}".format(response)
               )
 
         #plt.show()
@@ -69,12 +67,12 @@ def main():
     G.add_node(3, name="brain state")
     G.add_edge(2, 3)
     # MOTHERFUCKER redundance test
-    G.add_node(4, name="redundance test", redundance=proximity*sensi*valence)
+    if proximity == 1.0:
+        G.add_node(4, name="redundance test", response=proximity)
+    else:
+        G.add_node(4, name="redundance test", response=proximity*sensi*valence)
     G.add_edge(3, 4)
-    redundance = nx.get_node_attributes(G, 'redundance')[4] #* 5/9
-        #redundance = 1.0
-    G.add_node(5, name="behaviour")
-    G.add_edge(4, 5)
+    response = nx.get_node_attributes(G, 'response')[4]
     draw()
 
 
