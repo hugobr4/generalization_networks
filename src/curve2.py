2cf019092143a659b776f2bd307fee09416af822
Vini Salazar <viniws@gmail.com>
2018-11-11 22:39:47 -0200

    new plots

--- src/curve2.py
+++ src/curve2.py
@@ -12,14 +12,14 @@ def make_gauss(N, sig, mu):
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    plt.grid(alpha=0.5, linestyle = '--')
+    plt.grid(alpha=0.5, linestyle='--')
     '''
     x is the x axis limits, regarding the color spectrum
     s = Std dev, will be our conditioning coefficient
     n = mean which will be our max value
     '''
     x = np.arange(-40, 40, 0.01)
-    s = np.array([3, 6, 10])
+    s = np.array([10, 10, 10])
     m = [0, 0, 0]
     c = ['y', 'g', 'orange']
     n = s * (2.5, 3.0, 3.5)
@@ -27,7 +27,7 @@ def main():
 
     for n, sig, mu, color in zip(n, s, m, c):
         gauss = make_gauss(n, sig, mu)(x)
-        ax.plot(x, gauss, color, linewidth=2)
+        ax.plot(x, gauss, color, linewidth=4)
 
     plt.legend([f'Weak',
                 f'Medium',
